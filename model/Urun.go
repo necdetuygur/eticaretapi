513c939079799dabca9dedd3563f41f2421b36fc
Necdet UYGUR <necdet.uygur@gmail.com>
2021-10-31 13:28:13 +0300

    urun type tanımlandı

--- model/Urun.go
+++ model/Urun.go
@@ -1,25 +1,25 @@
 package model
 
 import (
-    "database/sql"
-    "eticaretapi/config"
+	"database/sql"
+	"eticaretapi/config"
 
-    _ "github.com/mattn/go-sqlite3"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 type Urun struct {
-    UrunID int    `json:"UrunID"`
-    Ad string `json:"Ad"`
-    Fiyat string `json:"Fiyat"`
-    Resim string `json:"Resim"`
-    Aciklama string `json:"Aciklama"`
-    KategoriID string `json:"KategoriID"`
+	UrunID     int     `json:"UrunID"`
+	Ad         string  `json:"Ad"`
+	Fiyat      float32 `json:"Fiyat"`
+	Resim      string  `json:"Resim"`
+	Aciklama   string  `json:"Aciklama"`
+	KategoriID int     `json:"KategoriID"`
 }
 
 func UrunCreateTable() {
-    db, _ := sql.Open("sqlite3", config.DB_NAME)
-    defer db.Close()
-    statement, _ := db.Prepare(`
+	db, _ := sql.Open("sqlite3", config.DB_NAME)
+	defer db.Close()
+	statement, _ := db.Prepare(`
         CREATE TABLE IF NOT EXISTS Urun
         (
             UrunID INTEGER PRIMARY KEY,
@@ -30,6 +30,6 @@ func UrunCreateTable() {
             KategoriID TEXT
         )
     `)
-    statement.Exec()
-    defer statement.Close()
+	statement.Exec()
+	defer statement.Close()
 }
